38e3e5335f5d5bf6678905476f3fa63efd47a02c
mengrj <mengrj.cs@gmail.com>
2024-09-09 14:09:10 +0000

    improve option

--- fuzzing/afl.sh
+++ fuzzing/afl.sh
@@ -5,5 +5,5 @@ cd afl-build || exit 1
 #cleanup
 rm -r -- *
 
-CC=${FUZZER_PATH}/afl-clang-fast cmake ../.. -DENABLE_FUZZING=On -DENABLE_SANITIZERS=On -DBUILD_SHARED_LIBS=Off -DAFL_FUZZ="${FUZZER_PATH}"/afl-fuzz
+CC=${CC} cmake ../.. -DENABLE_FUZZING=On -DENABLE_SANITIZERS=On -DBUILD_SHARED_LIBS=Off
 make afl-main
