a53475002976881412578685600f9bf835182a00
mengrj <mengrj.cs@gmail.com>
2024-09-10 01:29:39 +0000

    update build

--- fuzzing/afl.sh
+++ fuzzing/afl.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+# the first parameter
+CC=$1 # C compiler
+C_FLAGS=$2 # C flags
+
 mkdir -p afl-build || exit 1
 cd afl-build || exit 1
 #cleanup
 rm -r -- *
 
-CC=${CC} cmake ../.. -DENABLE_FUZZING=On -DENABLE_SANITIZERS=On -DBUILD_SHARED_LIBS=Off
+
+# if C_FLAGS is not empty
+if [ -n "$C_FLAGS" ]; then
+    CC=${CC} cmake ../.. -DENABLE_FUZZING=On -DENABLE_SANITIZERS=On -DBUILD_SHARED_LIBS=Off -DCMAKE_C_FLAGS="$C_FLAGS"
+    make afl-main
+    exit 0
+else
+    CC=${CC} cmake ../.. -DENABLE_FUZZING=On -DENABLE_SANITIZERS=On -DBUILD_SHARED_LIBS=Off
+fi
 make afl-main
