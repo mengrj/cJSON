12b49db1459c876afafd640ee24991d1708a3f3e
mengrj <mengrj.cs@gmail.com>
2024-09-27 08:36:07 +0000

    update building script

--- fuzzing/afl.sh
+++ fuzzing/afl.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
 
-# the first parameter
-CC=$1 # C compiler
-C_FLAGS=$2 # C flags
-
 mkdir -p afl-build || exit 1
 cd afl-build || exit 1
 #cleanup
 rm -r -- *
 
-
-# if C_FLAGS is not empty
-if [ -n "$C_FLAGS" ]; then
-    CC=${CC} cmake ../.. -DENABLE_FUZZING=On -DENABLE_SANITIZERS=On -DBUILD_SHARED_LIBS=Off -DCMAKE_C_FLAGS="$C_FLAGS"
-    make afl-main
-    exit 0
-else
-    CC=${CC} cmake ../.. -DENABLE_FUZZING=On -DENABLE_SANITIZERS=On -DBUILD_SHARED_LIBS=Off
-fi
+CC=clang cmake ../.. -DENABLE_FUZZING=On -DENABLE_SANITIZERS=On -DBUILD_SHARED_LIBS=Off  -DCMAKE_C_FLAGS="-g -O0"
 make afl-main
